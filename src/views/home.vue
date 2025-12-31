e29e7dd16b2bc76cfc654ef4b6f9855f96b4db64
zly321 <zhangliye0721@163.com>
2025-12-31 21:18:06 +0800

    首页添加背景图

--- src/views/home.vue
+++ src/views/home.vue
@@ -40,7 +40,7 @@ export default {
   min-height: 500px;
   text-align: center;
   /* 新增：背景图核心配置 */
-  background-image: url("~@/assets/img/首页.png"); /* 路径对应你的首页图片存放位置，务必确认图片名称一致 */
+  background-image: url("~@/assets/img/首页.png"); /* 路径对应首页图片存放位置，务必确认图片名称一致 */
   background-size: cover; /* 让背景图覆盖整个容器，保持比例不变形 */
   background-repeat: no-repeat; /* 禁止背景图重复平铺 */
   background-position: center center; /* 让背景图水平+垂直居中 */
